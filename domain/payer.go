3a44ee7707a73b4e62fffc1cb8b037cf941a7afb
aliasdhacker <andrewlanecarr@gmail.com>
2022-01-09 02:12:37 -0500

    Functionality working as expected.

--- domain/payer.go
+++ domain/payer.go
@@ -4,14 +4,9 @@ import (
 	"errors"
 	"github.com/google/uuid"
 	"log"
+	"strconv"
 	"time"
 )
-
-// Definition of a payer
-type Payer struct {
-	Payer  string
-	Points int
-}
 
 // Request wrapper for a points transaction
 type PointsTxnRequest struct {
@@ -34,17 +29,11 @@ type PointsTransaction struct {
 	Committed bool
 }
 
-// In memory storage for payers and points transactions
-var PayerByName map[string]*Payer
-var PointsTransactionByName map[string][]*PointsTransaction
-
-// Store the Payer in memory database.
-func StorePayer(payer Payer) {
-	PayerByName[payer.Payer] = &payer
-}
+// In memory storage - points transactions
+var PointsTransactionById map[uuid.UUID]*PointsTransaction
 
 // Store the transaction request in memory database (after converting it from the request wrapper and parsing time.)
-func StorePointsTransaction(txn PointsTxnRequest) (*PointsTransaction, error) {
+func StorePointsTransactionRequest(txn PointsTxnRequest) (*PointsTransaction, error) {
 	time, err := parseTime(txn.Timestamp)
 
 	if err != nil {
@@ -59,35 +48,101 @@ func StorePointsTransaction(txn PointsTxnRequest) (*PointsTransaction, error) {
 		Committed: false,
 	}
 
-	PointsTransactionByName[newT.Payer] = append(PointsTransactionByName[newT.Payer], &newT)
+	StorePointsTransaction(&newT)
 
 	return &newT, nil
 }
 
-// Process a transaction against the Payers ledger.
-func ProcessTransaction(txn *PointsTransaction) *Payer {
-	if txn.Committed {
-		return nil
-	}
+// Programmatic add of points transaction
+func StorePointsTransaction(txn *PointsTransaction) *PointsTransaction {
+	PointsTransactionById[txn.Id] = txn
 
-	payer := PayerByName[txn.Payer]
-	if payer == nil {
-		payer = &Payer{
-			Payer:  txn.Payer,
-			Points: 0,
-		}
-		PayerByName[txn.Payer] = payer
-	}
+	return txn
+}
 
-	PayerByName[txn.Payer].Points += txn.Points
+func GetOldestPoints(pointsTxns map[uuid.UUID]*PointsTransaction) *PointsTransaction {
+	var oldest *PointsTransaction
 
-	for _, t := range PointsTransactionByName[txn.Payer] {
-		if txn.Id == t.Id {
-			t.Committed = true
+	for _, i := range pointsTxns {
+		if oldest == nil || i.Timestamp.Before(oldest.Timestamp) {
+			oldest = i
 		}
 	}
 
-	return payer
+	return oldest
+}
+
+func CalculateSpend(request SpendRequest) ([]*PointsTransaction, error) {
+	var totalSpent int
+	var newTxns []*PointsTransaction
+	var allTransactions = make(map[uuid.UUID]*PointsTransaction)
+
+	for k, v := range PointsTransactionById {
+		allTransactions[k] = v
+	}
+
+	for request.Points > totalSpent {
+		txn := GetOldestPoints(allTransactions)
+		if txn == nil {
+			return nil, errors.New("Spent more points than available - only spending " + strconv.Itoa(totalSpent))
+		}
+		points := 0
+		if txn.Points > request.Points {
+			points = request.Points
+			request.Points = 0
+		} else {
+			points = txn.Points
+			request.Points -= txn.Points
+		}
+
+		newT := PointsTransaction{
+			Id:        uuid.New(),
+			Payer:     txn.Payer,
+			Points:    points * -1,
+			Timestamp: time.Now(),
+			Committed: true,
+		}
+
+		StorePointsTransaction(&newT)
+		totalSpent += points
+		delete(allTransactions, txn.Id)
+		newTxns = append(newTxns, &newT)
+	}
+
+	return GroupNewTxnsByName(newTxns), nil
+
+}
+
+func GroupNewTxnsByName(txns []*PointsTransaction) []*PointsTransaction {
+	var txnsGroupedByName []*PointsTransaction
+
+	for _, v := range txns {
+		if contains(txnsGroupedByName, v) {
+		} else {
+			copy := &PointsTransaction{
+				Id:        v.Id,
+				Payer:     v.Payer,
+				Points:    v.Points,
+				Timestamp: v.Timestamp,
+				Committed: v.Committed,
+			}
+			txnsGroupedByName = append(txnsGroupedByName, copy)
+		}
+	}
+
+	return txnsGroupedByName
+}
+
+// Test an array to see if it contains a value, if it does, add points
+func contains(container []*PointsTransaction, txn *PointsTransaction) bool {
+	for _, v := range container {
+		if v.Payer == txn.Payer {
+			v.Points += txn.Points
+			return true
+		}
+	}
+
+	return false
 }
 
 // Utility function to parse time strings in YYYY-MM-DDTHH:MM:SSZ format to golang Time structs
--- main.go
+++ main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/google/uuid"
 	"io"
 	"io/ioutil"
 	"log"
 	"needlepoint/domain"
 	"net/http"
+	"strconv"
 )
 
 /*
@@ -26,14 +28,19 @@ func getAllPayerBalances(w http.ResponseWriter, r *http.Request) {
 	//if err != nil {
 	//	return
 	//}
-	var output = "\n"
-	for _, payer := range domain.PayerByName {
-		payer := fmt.Sprintf("{ \"%s\": %d }\n", payer.Payer, payer.Points)
+	var first bool = true
+	var output = "\n{\n"
+	for _, payer := range domain.GroupNewTxnsByName(getArrayOfTransactions(domain.PointsTransactionById)) {
+		payer := fmt.Sprintf(" \"%s\": %d", payer.Payer, payer.Points)
+		if !first {
+			payer = ",\n" + payer
+		}
 		output = fmt.Sprintf("%s%s", output, payer)
+		first = false
 	}
 
 	w.WriteHeader(200)
-	io.WriteString(w, output)
+	io.WriteString(w, output+"\n}")
 
 	return
 }
@@ -45,26 +52,21 @@ Submit a points transaction
 Response example:
 Payer: &{Payer:DANNON Points:1100}
 
- */
+*/
 func newTransaction(w http.ResponseWriter, r *http.Request) {
 	var txn domain.PointsTxnRequest
-
 	var body []byte
-
 	body, _ = ioutil.ReadAll(r.Body)
 	json.Unmarshal(body, &txn)
-	newT, err := domain.StorePointsTransaction(txn)
+	newT, err := domain.StorePointsTransactionRequest(txn)
 
 	if err != nil {
-		//errString, _ := json.Marshal(err)
-
+		errString, _ := json.Marshal(err)
+		w.Write(errString)
 		return
 	}
 
-	p := domain.ProcessTransaction(newT)
-	if p != nil {
-		fmt.Fprintf(w, "Payer: %+v", p)
-	}
+	fmt.Fprintf(w, "Transaction: %+v", newT)
 
 	return
 }
@@ -73,25 +75,65 @@ func newTransaction(w http.ResponseWriter, r *http.Request) {
 
 Spend your points
 
- */
+*/
 func spend(w http.ResponseWriter, r *http.Request) {
-	output, err := json.MarshalIndent(&domain.PayerByName, "", "\t")
+	var spendRequest domain.SpendRequest
+	var output string
+	var body []byte
+	body, _ = ioutil.ReadAll(r.Body)
+	json.Unmarshal(body, &spendRequest)
+
+	responseTransactions, err := domain.CalculateSpend(spendRequest)
+	if err == nil {
+		output = formatSpendResponse(responseTransactions)
+	}
+
 	if err != nil {
+		io.WriteString(w, err.Error())
 		return
 	}
 
-	w.Write(output)
-
+	io.WriteString(w, output)
 	return
 }
 
 func main() {
-	domain.PointsTransactionByName = make(map[string][]*domain.PointsTransaction)
-	domain.PayerByName = make(map[string]*domain.Payer)
+	domain.PointsTransactionById = make(map[uuid.UUID]*domain.PointsTransaction)
 	mux := http.NewServeMux()
 	mux.HandleFunc("/newTxn", newTransaction)
 	mux.HandleFunc("/", getAllPayerBalances)
+	mux.HandleFunc("/spend", spend)
 
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
+}
+
+// Helper func to convert map to array
+func getArrayOfTransactions(txns map[uuid.UUID]*domain.PointsTransaction) []*domain.PointsTransaction {
+	var arr []*domain.PointsTransaction
+	for _, v := range txns {
+		arr = append(arr, v)
+	}
+
+	return arr
+}
+
+// Helper func - make response match expected response
+func formatSpendResponse(transactions []*domain.PointsTransaction) string {
+	var output string = "\n[\n"
+	var first bool = true
+
+	for _, v := range transactions {
+		if !first {
+			output += ",\n{"
+		} else {
+			output += "{"
+		}
+		output += " \"payer\": \"" + v.Payer + "\", \"points\": " + strconv.Itoa(v.Points) + " }"
+		first = false
+	}
+
+	output += "\n]\n"
+
+	return output
 }
